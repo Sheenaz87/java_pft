e8b438c46aba87d0af43cf805a4be2eda16e9996
sheenaz <sheenaz@yandex.ru>
2020-02-13 20:58:10 +0300

    Добавлено решение по задаче 2 (функция для расстояния между точками)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,17 +1,24 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+import org.w3c.dom.ls.LSOutput;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
             hello("world");
         hello("user");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Point p1 = new Point (4,5);
+        Point p2 = new Point (5,6);
+      System.out.println("Расстояние между точками " + p1 + " и " + p2 + " = " + distance(p1,p2));
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4,6);
+      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
@@ -19,12 +26,19 @@ public class MyFirstProgram {
 
     }
 
-    public static double area(double len) {
-        return len * len;
-    }
+  public static double distance(Point p1, Point p2) {
+      return Math.sqrt(p1.x * p1.y + p2.x * p2.y);
 
-    public static double area(double a, double b) {
-        return a * b;
-    }
+  }
+
+
+
+
+
+
+
+
+
+
 
 }
