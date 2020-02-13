d6482a2b744cba81e4ffe9c77454b08817235703
sheenaz <sheenaz@yandex.ru>
2020-02-13 21:42:57 +0300

    Расчет расстояния между точками + класс

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,34 +6,17 @@ import org.w3c.dom.ls.LSOutput;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-            hello("world");
-        hello("user");
 
-        Point p1 = new Point (4,5);
-        Point p2 = new Point (5,6);
-      System.out.println("Расстояние между точками " + p1 + " и " + p2 + " = " + distance(p1,p2));
-
-
-        Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-        Rectangle r = new Rectangle(4,6);
-      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-    }
-
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody + "!");
+        Point p1 = new Point (1,4);
+        Point p2 = new Point (2,6);
+      System.out.println("Расстояние между двумя точками  = " + distance(p1,p2));
 
     }
 
   public static double distance(Point p1, Point p2) {
-      return Math.sqrt(p1.x * p1.y + p2.x * p2.y);
+    return Math.sqrt((p1.y - p1.x) * (p1.y - p1.x) + (p2.y - p2.x) * (p2.y - p2.x));
 
   }
-
-
-
-
 
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -11,6 +11,7 @@ public class Point{
     this.y = y;
   }
 
+
   }
 
 
