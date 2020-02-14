1ea3a3d491dbe5751558bc5bf2611460b4ccf3cb
sheenaz <sheenaz@yandex.ru>
2020-02-14 13:35:24 +0300

    решение задания 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,9 +7,15 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point (1,4);
-        Point p2 = new Point (2,6);
-      System.out.println("Расстояние между двумя точками  = " + distance(p1,p2));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point (1,4);
+    Point p2 = new Point (2,6);
+    System.out.println("Расстояние между двумя точками  = " + distance(p1,p2));
 
     }
 
