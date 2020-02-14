3a5dba8000372ecae4a137aed3519d1373be03a9
sheenaz <sheenaz@yandex.ru>
2020-02-14 15:34:37 +0300

    Выполнение задания 2 (расстояние между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,20 +1,21 @@
 package ru.stqa.pft.sandbox;
 
-public class Point{
+public class Point {
 
   public double x;
   public double y;
 
-  public Point (double x, double y) {
+  public Point(double x, double y) {
 
     this.x = x;
     this.y = y;
   }
 
-
+  public double distance(Point p2) {
+    return Math.sqrt((this.y - this.x) * (this.y - this.x) + (p2.y - p2.x) * (p2.y - p2.x));
   }
 
-
+}
 
 
 
