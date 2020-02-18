86506b1f0c62b5345b85700ae3760f30fec2ad7d
sheenaz <sheenaz@yandex.ru>
2020-02-18 23:14:42 +0300

    Задание 4. Добавлен тест для создания контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String midddlename;
+  private final String lastname;
+  private final String nickname;
+  private final String adress;
+  private final String homephone;
+  private final String workphone;
+  private final String email;
+
+  public ContactData(String name, String midddlename, String lastname, String nickname, String adress, String homephone, String workphone, String email) {
+    this.name = name;
+    this.midddlename = midddlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.adress = adress;
+    this.homephone = homephone;
+    this.workphone = workphone;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getMidddlename() {
+    return midddlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getAdress() {
+    return adress;
+  }
+
+  public String getHomephone() {
+    return homephone;
+  }
+
+  public String getWorkphone() {
+    return workphone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
