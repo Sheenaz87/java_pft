4dd0f0e582d5b66848ef632e6b68469a389e7d58
sheenaz <sheenaz@yandex.ru>
2020-02-18 23:16:54 +0300

    Исправлена опечатка в классе Групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -35,7 +35,7 @@ public class GroupCreationTests {
   public void testGroupCreation() throws Exception {
     gotoGroupPage();
     initGroupCreation();
-    fillGroupForm(new DroupData("test1", "test2", "test3"));
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
     submitGroupCreation();
     returntoGroupPage();
 
@@ -49,16 +49,16 @@ public class GroupCreationTests {
     wd.findElement(By.name("submit")).click();
   }
 
-  private void fillGroupForm(DroupData droupData) {
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(droupData.getName());
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(droupData.getHeader());
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(droupData.getFooter());
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
   private void initGroupCreation() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.addressbook;
 
-public class DroupData {
+public class GroupData {
   private final String name;
   private final String header;
   private final String footer;
 
-  public DroupData(String name, String header, String footer) {
+  public GroupData(String name, String header, String footer) {
     this.name = name;
     this.header = header;
     this.footer = footer;
